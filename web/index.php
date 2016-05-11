eb3051c1b5f23e8454211dae6123506a2d42f8b9
Robert <axius_robert@yahoo.com>
2016-05-11 14:55:49 +0300

    sqlConnect added

--- web/index.php
+++ web/index.php
@@ -12,4 +12,15 @@ $app->get('/test',function (){
     return "WhAT?";}
     );
 
+$app->get('/sqlConnect', function (){
+    $link = mysqli_connect('localhost', 'root', 'cozacu','mysql');
+        if (!$link) {
+        die('Failed to connect: ' . mysql_error());
+        }
+    $returnMessage = 'Connection succesful!';
+    #mysql_close($link); object given in /var/www/html/TestProject/web/index.php
+    return $returnMessage;
+    });
+
+    
 $app->run();
