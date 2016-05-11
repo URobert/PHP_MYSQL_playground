11454a2c9801af830dc89a8ade47ed75a0561f70
Robert <axius_robert@yahoo.com>
2016-05-11 15:49:44 +0300

    County & City filtererd import

--- web/index.php
+++ web/index.php
@@ -50,5 +50,64 @@ $app->get('/addCounties', function (){
             }       
         }
         return $returnMessage;
-});  
+});
+
+//SQL FILTERED County-City import (v2)
+$app->get('/countyCityImport', function (){
+    //CONNECTING TO DB
+    $returnMessage = "FFFFFFF.......";
+    $connect = mysqli_connect('localhost', 'root', 'cozacu','test1');
+    if (!$connect) {
+        die('Connection failed!' . mysql_error());
+    }
+    $returnMessage = "Connection was succesfull" . "<br>";
+    
+    //GETTING FILE CONTENT (TO BE IMPORTED) AND SEPARATING LINES
+       $fileContent = file_get_contents("orase_judete.csv");
+       $lines = explode("\n", $fileContent);
+    
+        foreach ($lines as $line){
+            $elements = explode(",", $line);
+            
+            $checkCounty = "SELECT * FROM county WHERE name=". "'".trim($elements[1])."' limit 1";
+            $resultCounty = $connect->query($checkCounty);
+            if ($resultCounty){
+                $temp_county_ID = $resultCounty->fetch_assoc()["id"];
+                    if($temp_county_ID){
+                        //ADD THE CITY INTO CITIES WITH GIVEN COUNTY_ID;
+                        $checkCity = "SELECT * FROM city WHERE name="."'".trim($elements[0])."' limit 1";
+                        $resultCity = $connect->query($checkCity)->fetch_assoc();
+                        
+                        if ($resultCity){
+    $returnMessage .= $elements[0] . " already exists." . "<br>";
+    
+                        }else{ //CITY NOT IN DB
+                            $addNewCity = 'INSERT INTO city (county_id,name) VALUES ('
+                             . $temp_county_ID . ','
+                           ."'"  . trim($elements[0]) . "'" .')';
+                           if ($connect->query($addNewCity) === TRUE) {
+    $returnMessage .="City sucessfuly imported:" . $elements[0] . "<br>";
+                           }
+                        }
+                                         
+                    }else{ //COUNTY NOT IN DB
+    $returnMessage .= $elements[1] . "County NOT FOUND." . "<br>";
+                        $addNewCounty = 'INSERT INTO county (name) VALUES ('
+                           ."'"  . trim($elements[1]) . "'" .')';
+                           if ($connect->query($addNewCounty) === TRUE) {
+    $returnMessage .= "County sucessfuly imported:" . $elements[1] . "<br>";
+                           }
+                           //ADD THE CITY
+                           $temp_county_ID = mysqli_insert_id($connect);
+                           $addNewCity = 'INSERT INTO city (county_id,name) VALUES ('
+                             . $temp_county_ID . ','
+                           ."'"  . trim($elements[0]) . "'" .')';
+                           if ($connect->query($addNewCity) === TRUE) {
+    $returnMessage .= "City sucessfuly imported:" . $elements[0] . "<br>";
+                           }
+                        }
+            }
+        }
+    return $returnMessage;
+});
 $app->run();
