755cbfa23ba17aecaee6c450786b5692ea44fafc
URobert <axius_robert@yahoo.com>
2016-05-24 16:19:19 +0300

    Add module updated, delete started

--- views/manualCountyAdd.php
+++ views/manualCountyAdd.php
@@ -1,11 +1,27 @@
 <html>
     <body>
         <form action="/counties/add" method="post">
+          
+        <!-- get county list from DB -->
+        <?php
+        include_once 'Add.php';
+        $requestCountyList = "SELECT * FROM county";
+        $returedList = $this->connect->query($requestCountyList);
+        var_dump($returedList);    
+              
+        ?>  
           County:
-          <input type="text" name="county"> 
+          <select name='countyid'>
+            <option value='0'>-select county-</option>
+            <option value='1'>Bihor</option>
+            <option value='1'>Bihor</option>
+            <option value='1'>Bihor</option>
+            <option value='1'>Bihor</option>
+          </select>
         
+         <input type="text" name="county" id="county">
           City:
-          <input type="text" name="city">
+          <input type="text" name="city" id="city">
           <input type="submit" value="Add">
         </form>
         
@@ -15,11 +31,17 @@
 
 
 <style>
-
     form{
         text-align: center;
         margin-top:50px;
     }
-    
-
 </style>
+
+<!--<script language=javascript>
+    console.log($("#county").val());
+    if (($("#county").val()).length < 3) {
+        alert("None of the fields can be empty. Use full names for both citites and counties.");
+    }else{
+        console.log("script goes on");
+    }
+</script>-->
--- views/manualCountyDelete.php
+++ views/manualCountyDelete.php
@@ -7,20 +7,28 @@
                 <th>CityID</th>
                 <th>City</th>
             </thead>
-                
+                <?php $buttonID= "";  ?>
                 <?php while ($entry = $fullConent->fetch_assoc()): ?>
+                <?php $buttonID = $entry['name'] ?>
                 <tr>
                     <td><?= $entry['id'] ?></td>
                     <td><?= $entry['name'] ?></td>
-                    <td><button class='btn btn-primary'>Delete</button></td>
+                    <td><button class='btn btn-danger deleteButton' id='<?php echo $buttonID?>'>Delete</button></td>
                 </tr>
                 <?php endwhile; ?>
-
+        
 </table>
 </div>
 </html>
 
 <style>
-    .wrapper{
-        margin-left: 45%;
+.wrapper{
+    margin-left: 45%;
+}
+
+.btn-danger, btn {
+    padding: 5px;
+}
+
 </style>
+              	
