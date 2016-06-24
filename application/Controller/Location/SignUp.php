b2c84b5ba1bc2d9a41948aa1ffc9be46ee51a90f
URobert <axius_robert@yahoo.com>
2016-06-24 16:32:04 +0300

    Login works with simple passwords. Site navigation must be made available for all users - require fix

--- application/Controller/Location/SignUp.php
+++ application/Controller/Location/SignUp.php
@@ -13,7 +13,27 @@ class SignUp extends \TestProject\Controller\BaseController
         {
                 if ($request->getMethod()== "POST"){
                         if ($_POST['password'] === $_POST['passwordVerify']){
-                                $addUser = "INSERT INTO user (username,password,email,status) VALUES ('Roberto','1111', 'ahoy@yahoo.com', 1)";
+                                $username = $_POST['username'];
+                                $password = $_POST['password'];
+                                $email = $_POST['emailAddress'];
+                                if ($_POST['status'] = 'active'){
+                                        $status = 1;
+                                }else{
+                                        $status = 0;
+                                }
+                                $addUser = "INSERT INTO user (username,password,email,status)
+                                VALUES ('$username','$password', '$email', $status)";
+                                if ($this->connect->query($addUser) === TRUE){
+echo "User was added.";
+                                $getId = "SELECT id from user where username='$username'";
+                                $result = $this->connect->query($getId);
+                                $id = $result->fetch_assoc()['id'];
+                                echo $id;
+                                $_SESSION['userId'] = $id;
+                                }
+                                
+                        }else{
+                                echo "Passwords do not match.";
                         }
                 }
                 return $this->render();
